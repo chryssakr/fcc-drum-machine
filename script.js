b9a77f1b2a0a8310b1bbcf4772acb3df8fcc53cd
Chryssa Karatza <chryssakaratza@gmail.com>
2023-06-13 15:53:21 +0100

    prettify code

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-const drumPads = document.querySelectorAll('.drum-pad');
+const drumPads = document.querySelectorAll(".drum-pad");
 
-drumPads.forEach(pad => {
-  pad.addEventListener('click', () => {
-    const audio = pad.querySelector('audio');
+drumPads.forEach((pad) => {
+  pad.addEventListener("click", () => {
+    const audio = pad.querySelector("audio");
     audio.currentTime = 0;
     audio.play();
-    document.querySelector('#display').innerText = pad.id;
+    document.querySelector("#display").innerText = pad.id;
   });
 });
 
-drumPads.forEach(pad => {
-  document.addEventListener('keydown', (e) => {
-    
-    const audio = pad.querySelector('audio');
+drumPads.forEach((pad) => {
+  document.addEventListener("keydown", (e) => {
+    const audio = pad.querySelector("audio");
     if (e.key.toUpperCase() === audio.id) {
-      const audio = pad.querySelector('audio');
+      const audio = pad.querySelector("audio");
       audio.currentTime = 0;
       audio.play();
-      document.querySelector('#display').innerText = pad.id;
-      pad.classList.add('active');
+      document.querySelector("#display").innerText = pad.id;
+      pad.classList.add("active");
     }
-  })
-})
+  });
+});
 
-drumPads.forEach(pad => {
-  document.addEventListener('keyup', (e) => {
-    pad.classList.remove('active');
-  })
-})
+drumPads.forEach((pad) => {
+  document.addEventListener("keyup", (e) => {
+    pad.classList.remove("active");
+  });
+});
